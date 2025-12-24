fb5a25c319dacfdf55c44a712cbb22ef9a334141
ChichMeat <90647843+ChichMeat@users.noreply.github.com>
2025-12-24 18:25:50 +0300

    Create shared_config.py
    dev1: Обновил конфиг для продакшена

---
+++ backend/backend/shared_config.py
@@ -0,0 +1,13 @@
+DATABASE_SETTINGS = {
+    'host': 'postgres.vetclinic.ru',  
+    'port': 5432,
+    'name': 'vetclinic_production',   
+    'user': 'vet_admin'               
+
+API_KEYS = {
+    'sms_service': 'default_key_123'
+}
+
+
+DEBUG_MODE = False
+LOG_LEVEL = 'INFO'
